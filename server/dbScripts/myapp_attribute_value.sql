bf24ec388942837cd7ab2d4f073b6d3a999cde94
Nguyen Quoc Huy <ongdo2004@gmail.com>
2018-04-20 00:05:20 +0700

    #update: - cập nhật lại script db: bỏ insert

--- server/dbScripts/myapp_attribute_value.sql
+++ server/dbScripts/myapp_attribute_value.sql
@@ -9,7 +9,3 @@ CREATE TABLE myapp.attribute_value
     value varchar(255),
     last_modified_time datetime
 );
-INSERT INTO myapp.attribute_value (id, version, is_deleted, attribute_id, last_modified_user, reference_id, value, last_modified_time) VALUES ('001', 0, false, '001', 'admin', '07dfe7d1-ba9e-4e8b-89ac-52cfc329c1aa', '1_2', '2018-03-31 14:10:16');
-INSERT INTO myapp.attribute_value (id, version, is_deleted, attribute_id, last_modified_user, reference_id, value, last_modified_time) VALUES ('002', 0, false, '002', 'admin', '07dfe7d1-ba9e-4e8b-89ac-52cfc329c1aa', '2K', '2018-03-31 14:10:16');
-INSERT INTO myapp.attribute_value (id, version, is_deleted, attribute_id, last_modified_user, reference_id, value, last_modified_time) VALUES ('003', 0, false, '001', 'admin', '12ff3c0c-10ae-44c5-80cb-1507bf709c78', '1_1', '2018-03-31 14:10:16');
-INSERT INTO myapp.attribute_value (id, version, is_deleted, attribute_id, last_modified_user, reference_id, value, last_modified_time) VALUES ('004', 0, false, '002', 'admin', '12ff3c0c-10ae-44c5-80cb-1507bf709c78', '2K', '2018-03-31 14:10:16');
