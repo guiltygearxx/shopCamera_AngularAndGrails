7ca74a1a9a28b49af535dc75c5da1e334bf38298
Nguyen Quoc Huy <ongdo2004@gmail.com>
2018-04-28 16:40:38 +0700

    #fix: - scroll về đầu trang khi thực hiện chuyển trang. - menu-nav không thực hiện dock khi refresh trang.

--- client2/src/app/app.component.ts
+++ client2/src/app/app.component.ts
@@ -1,17 +1,24 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {ToasterConfig} from "angular2-toaster";
 
 declare var $: any;
+
+declare var backToTop: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
   toasterConfig: ToasterConfig = new ToasterConfig({
 
     positionClass: 'toast-top-right'
   });
+
+  ngAfterViewInit(): void {
+
+    backToTop();
+  }
 }
